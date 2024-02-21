eee15eb549b21bd929fc34ac13f3319c58154ddc
Artem Iliushin <thatsforsure@mail.ru>
2024-02-21 14:11:46 +0300

    added a template for articles

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('article')
+    title = 'Articles'
+    description = 'Project by nerodnoy'
+    return render(
+        request,
+        'articles.html',
+        context={
+            'title': title,
+            'description': description
+        }
+    )
