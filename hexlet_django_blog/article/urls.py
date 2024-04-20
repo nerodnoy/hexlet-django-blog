980234f625d306aa2017173f47631cf08935ef94
Artem Iliushin <thatsforsure@mail.ru>
2024-04-20 12:55:53 +0300

    routes test

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.IndexView.redirect_to_article, name='home'),
+    path('articles/<str:tags>/<int:article_id>/', views.IndexView.as_view(), name='article'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,10 +1,12 @@
+from django.urls import reverse
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 class IndexView(View):
-    def get(self, request):
-        return render(request, 'articles/index.html')
+    def get(self, request, tags=None, article_id=None):
+        article_text = f"Статья номер {article_id}. Тег {tags}"
+        return render(request, 'articles/index.html', {'article_text': article_text})
 
-    # def get(self, request, *args, **kwargs):
-    #     return HttpResponse('Hello, World!')
+    def redirect_to_article(request):
+        return redirect(reverse('article', kwargs={'tags': 'python', 'article_id': 42}))
